77a2942d3506c48d8daada8ef3a05c2358eb87ec
Augustinas Malinauskas <augustinasmal@gmail.com>
2023-12-21 17:10:40 +0000

    feat(chat): speech recognition and http fix

---
+++ Enchanted/UI/Views/Chat/Components/Recorder/RecordingView.swift
@@ -0,0 +1,65 @@
+//
+//  SwiftUIView.swift
+//  Enchanted
+//
+//  Created by Augustinas Malinauskas on 18/12/2023.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct RecordingView: View {
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @Binding var isRecording: Bool
+    var onComplete: (_ transcription: String) -> () = {_ in}
+    
+    private func toggleRecord() {
+        Task {
+            await speechRecognizer.userInit()
+            await toggleTranscribing()
+        }
+    }
+    
+    private func toggleTranscribing() async {
+        if isRecording {
+            speechRecognizer.stopTranscribing()
+            onComplete(speechRecognizer.transcript)
+            isRecording = false
+        } else {
+            speechRecognizer.resetTranscript()
+            speechRecognizer.startTranscribing()
+            isRecording = true
+        }
+    }
+    
+    var body: some View {
+        Button(action: toggleRecord) {
+            if isRecording {
+                ZStack {
+                    Color(.systemBlue)
+                    
+                    Image(systemName: "square.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(Color(.systemBackground))
+                        .frame(width: 8)
+                }
+                .clipShape(Circle())
+                .frame(width: 20, height: 20)
+            } else {
+                Image(systemName: "waveform")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 20)
+                    .foregroundStyle(Color(.systemGray))
+            }
+        }
+    }
+}
+
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordingView(isRecording: .constant(true))
+    }
+}
--- Enchanted/UI/Views/Main/MainView.swift
+++ Enchanted/UI/Views/Main/MainView.swift
@@ -75,23 +75,6 @@ struct MainView: View {
     }
 }
 
-extension View {
-    func getRect() -> CGRect {
-        return UIScreen.main.bounds
-    }
-    
-    func safeArea() -> UIEdgeInsets {
-        let null = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
-            return null
-        }
-        guard let safeArea = screen.windows.first?.safeAreaInsets else {
-            return null
-        }
-        return safeArea
-    }
-}
-
 #Preview {
     MainView()
 }
