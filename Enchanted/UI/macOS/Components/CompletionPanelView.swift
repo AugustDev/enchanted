bf31f1abc4ff75cd5ea9feb856084e988e87a8f1
Augustinas Malinauskas <augustinasmal@gmail.com>
2024-05-01 07:24:34 -0700

    feat: custom completions (#93)

--- Enchanted/UI/macOS/Components/CompletionPanelView.swift
+++ Enchanted/UI/macOS/Components/CompletionPanelView.swift
@@ -31,6 +31,11 @@ struct PanelCompletionsView: View {
     @State var completionMode: CompletionsPromptMode = .completionsInApp
     @State var selectedCompletion: CompletionInstructionSD? = nil
     
+    @State var customCompletionInstruction: String = ""
+    @State var showCustomCompletionInstructionTextField = false
+    @FocusState var focusCustomCompletionsTectField: Bool
+    @Namespace var animation
+    
     var filetedCompletions: [CompletionInstructionSD] {
         if let selectedCompletion = selectedCompletion {
             return [selectedCompletion]
@@ -38,24 +43,93 @@ struct PanelCompletionsView: View {
         return completions
     }
     
-    let customCompletionText: some View = Button(action: {}) {
-        HStack {
-            Text("TAB")
-                .textCase(.uppercase)
-                .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(5)
-                .font(.system(size: 10, weight: .medium, design: .default))
+    func changeCompletionMode() {
+        withAnimation {
+            completionMode = completionMode.next
+        }
+    }
+    
+    @MainActor
+    func submitCompletion(_ completion: CompletionInstructionSD) {
+        withAnimation {
+            selectedCompletion = completion
+            switch completionMode {
+            case .completionsInCurrentWindow:
+                completionInWindow(completion, false)
+            case .completionsInApp:
+                completionInApp(completion)
+            }
+        }
+    }
+    
+    var customCompletionButton: some View {
+        Button(action: {
+            withAnimation(.easeOut(duration: 0.15)) {
+                showCustomCompletionInstructionTextField = true
+                focusCustomCompletionsTectField = true
+            }
+        }) {
+            HStack {
+                Text("TAB")
+                    .textCase(.uppercase)
+                    .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(5)
+                    .font(.system(size: 10, weight: .medium, design: .default))
+                
+                Text("Your Command")
+                    .enchantify()
+            }
+            .padding(.vertical, 4)
+            .padding(.horizontal, 8)
+            .foregroundStyle(.label)
+            .background(RoundedRectangle(cornerRadius: 5).fill(.bgCustom))
             
-            TextField("TAB", text: .constant("x"))
-                .font(.system(size: 12))
+        }
+        .buttonStyle(GrowingButton())
+        .keyboardShortcut(.tab, modifiers: [])
+        .matchedGeometryEffect(id: "customCommand", in: animation)
+    }
+    
+    var customCompletionInstructionTextField: some View {
+        HStack {
+            Button(action: {
+                withAnimation(.easeOut(duration: 0.15)) {
+                    showCustomCompletionInstructionTextField = false
+                }
+            }) {
+                Text("TAB")
+                    .textCase(.uppercase)
+                    .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(5)
+                    .font(.system(size: 10, weight: .medium, design: .default))
+            }
+            .buttonStyle(GrowingButton())
+            .keyboardShortcut(.tab, modifiers: [])
+            
+            TextField("Instruction", text: $customCompletionInstruction)
+                .textFieldStyle(.plain)
+                .focused($focusCustomCompletionsTectField)
+                .onSubmit {
+                    let completion = CompletionInstructionSD(
+                        name: "Custom Command",
+                        keyboardCharacterStr: "",
+                        instruction: customCompletionInstruction + "\n\n",
+                        order: 0)
+                    
+                    DispatchQueue.main.async {
+                        submitCompletion(completion)
+                    }
+                }
         }
         .padding(.vertical, 4)
         .padding(.horizontal, 8)
         .foregroundStyle(.label)
         .background(RoundedRectangle(cornerRadius: 5).fill(.bgCustom))
+        .matchedGeometryEffect(id: "customCommand", in: animation)
+        .padding(.bottom, 10)
     }
-        .buttonStyle(GrowingButton())
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -81,24 +155,26 @@ struct PanelCompletionsView: View {
                 }
                 .showIf(selectedCompletion != nil)
             }
-            WrappingHStack(alignment: .leading) {
-                ForEach(filetedCompletions) { completion in
-                    CompletionButtonView(
-                        name: completion.name,
-                        keyboardCharacter: completion.keyboardCharacter,
-                        action: {
-                            withAnimation {
-                                selectedCompletion = completion
-                                switch completionMode {
-                                case .completionsInCurrentWindow:
-                                    completionInWindow(completion, false)
-                                case .completionsInApp:
-                                    completionInApp(completion)
-                                }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                WrappingHStack(alignment: .leading) {
+                    ForEach(filetedCompletions) { completion in
+                        CompletionButtonView(
+                            name: completion.name,
+                            keyboardCharacter: completion.keyboardCharacter,
+                            action: {
+                                submitCompletion(completion)
                             }
-                        }
-                    )
-                    .keyboardShortcut(KeyEquivalent(completion.keyboardCharacter), modifiers: [])
+                        )
+                        .keyboardShortcut(KeyEquivalent(completion.keyboardCharacter), modifiers: [])
+                    }
+                    
+                    customCompletionButton
+                        .showIf(!showCustomCompletionInstructionTextField)
+                }
+                
+                if showCustomCompletionInstructionTextField {
+                    customCompletionInstructionTextField
                 }
             }
             .padding(.bottom, 10)
@@ -111,28 +187,39 @@ struct PanelCompletionsView: View {
                     Text("Respond in current **Window**.")
                 }
                 
-                Text("SPACE")
-                    .font(.caption2)
-                    .padding(.horizontal, 7)
-                    .padding(.vertical, 2)
-                    .background(RoundedRectangle(cornerRadius: 3).fill(.bgCustom))
+                Button(action: changeCompletionMode) {
+                    HStack(spacing: 4) {
+                        Text("⇧")
+                            .font(.caption2)
+                            .padding(.horizontal, 7)
+                            .padding(.vertical, 2)
+                            .background(RoundedRectangle(cornerRadius: 3).fill(.bgCustom))
+                        
+                        Text("+")
+                            .font(.footnote)
+                        
+                        Text("SPACE")
+                            .font(.caption2)
+                            .padding(.horizontal, 7)
+                            .padding(.vertical, 2)
+                            .background(RoundedRectangle(cornerRadius: 3).fill(.bgCustom))
+                    }
+                        
+                }
+                .buttonStyle(GrowingButton())
                 
                 Text("to switch")
-                
             }
             
             .padding(.horizontal, 8)
             .showIf(selectedCompletion == nil)
-            
         }
         .padding()
         .background {
             RoundedRectangle(cornerRadius: 8).fill(.ultraThickMaterial)
         }
-        .onKeyboardShortcut(key: .space, modifiers: []) {
-            withAnimation {
-                completionMode = completionMode.next
-            }
+        .onKeyboardShortcut(key: .space, modifiers: [.shift]) {
+            changeCompletionMode()
         }
         .frame(minWidth: 500, maxWidth: 500)
     }
